94e6db2ce49b6361f5f7e3380a934c356b6f1670
Alexander Trapp <atrapp@MBATR.local>
2018-11-27 12:52:57 +0100

    Exercise: add resilience and caching to delete-, getsingle- and update-commands

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetSingleBusinessPartnerByIdCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetSingleBusinessPartnerByIdCommand.java
@@ -1,5 +1,9 @@
 package com.sap.cloud.s4hana.examples.addressmgr.commands;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.sap.cloud.sdk.cloudplatform.cache.CacheKey;
+import com.sap.cloud.sdk.s4hana.connectivity.CachingErpCommand;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
 import org.slf4j.Logger;
 
@@ -7,18 +11,28 @@ import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 
-public class GetSingleBusinessPartnerByIdCommand {
+import javax.annotation.Nonnull;
+import java.util.concurrent.TimeUnit;
+
+public class GetSingleBusinessPartnerByIdCommand extends CachingErpCommand<BusinessPartner> {
     private static final Logger logger = CloudLoggerFactory.getLogger(GetSingleBusinessPartnerByIdCommand.class);
+
+    public static final Cache<CacheKey, BusinessPartner> cache = CacheBuilder.newBuilder()
+            .maximumSize(10)
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .build();
 
     private final BusinessPartnerService service;
     private final String id;
 
     public GetSingleBusinessPartnerByIdCommand(final BusinessPartnerService service, final String id) {
+        super(GetSingleBusinessPartnerByIdCommand.class);
         this.service = service;
         this.id = id;
     }
 
-    public BusinessPartner execute() throws Exception {
+    @Override
+    protected BusinessPartner runCacheable() throws Exception {
         return service.getBusinessPartnerByKey(id)
                 .select(BusinessPartner.BUSINESS_PARTNER,
                         BusinessPartner.LAST_NAME,
@@ -36,5 +50,16 @@ public class GetSingleBusinessPartnerByIdCommand {
                                 BusinessPartnerAddress.HOUSE_NUMBER
                         ))
                 .execute();
+    }
+
+    @Nonnull
+    @Override
+    protected Cache<CacheKey, BusinessPartner> getCache() {
+        return cache;
+    }
+
+    @Override
+    protected CacheKey getCommandCacheKey() {
+        return CacheKey.ofTenantAndUserIsolation().append(id);
     }
 }
